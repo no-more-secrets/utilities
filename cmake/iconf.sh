d92682b367a5d0006cc028a061abbb26fcdc627a
David P. Sicilia <dpsicilia@gmail.com>
2019-05-31 11:12:02 -0400

    Add iconf.sh

--- cmake/iconf.sh
+++ cmake/iconf.sh
@@ -9,4 +9,8 @@ dialog --yesno "Run clang-tidy?" 10 30
 
 dialog --menu "Choose Compiler" 15 40 6 default "default on the system" gcc "" clang ""
 
-dialog --prgbox "cmc --verbose --no-color" 40 80
+dialog --prgbox "cmc c --verbose --no-color --clang --lld" 60 80
+
+dialog --prgbox "make -j12" 60 80
+
+dialog --prgbox "make test" 60 80
