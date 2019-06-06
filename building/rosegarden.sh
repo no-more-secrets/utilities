818acb60aa027b07e6dcaff9dd48baf2745d7494
David P. Sicilia <dpsicilia@gmail.com>
2019-06-05 23:14:51 -0400

    Add rosegarden building script

--- building/rosegarden.sh
+++ building/rosegarden.sh
@@ -1,30 +1,130 @@
 #!/bin/bash
-# This script will checkout and build the latest tag of Rosegarden.
+# ---------------------------------------------------------------
+# Rosegarden
+# ---------------------------------------------------------------
+# This script will build the latest tag of Rosegarden.
 set -e
 set -o pipefail
 
-script_dir=$(cd $(dirname $0) && pwd)
-echo "script directory: $script_dir"
+# Must be done first.
+this=$(cd $(dirname $0) && pwd)
+cd $this
 
-. $script_dir/util.sh
+# ---------------------------------------------------------------
+# Includes
+# ---------------------------------------------------------------
+source util.sh
 
-sudo apt install libjack-dev    \
-                 ladspa-sdk-dev \
-                 dssi-dev       \
-                 liblo-dev      \
-                 liblrdf-dev
+# ---------------------------------------------------------------
+# Initialization
+# ---------------------------------------------------------------
+# This must be a lowercase short word with no spaces describing
+# the software being built. Folders/links will be named with
+# this.
+project_key="rosegarden"
 
-latest_tag=$(svn ls http://svn.code.sf.net/p/rosegarden/code/tags | grep rosegarden- | sort -V | tail -n1)
-echo "latest tag: $latest_tag"
+cd /tmp
 
-svn co http://svn.code.sf.net/p/rosegarden/code/tags/$latest_tag
+work=$project_key-build
+[[ -d $work ]] && /bin/rm -rf "$work"
+mkdir -p $work && cd $work
 
-cd $latest_tag
+tools="$HOME/dev/tools"
+mkdir -p "$tools"
 
+# ---------------------------------------------------------------
+# Functions
+# ---------------------------------------------------------------
+latest_svn_repo_tag() {
+    local repo="$1"
+    # "rosegarden-19.06/" --> "19.06"
+    svn ls "$repo" | grep rosegarden-                  \
+                   | sort -V                           \
+                   | sed -r 's/rosegarden-(.*)\//\1/g' \
+                   | tail -n1
+}
+
+checkout_latest_tag() {
+    local repo="$1"
+    local tag="$2"
+    svn co $repo/$tag
+}
+
+supplemental_install() {
+    supp_inst="$this/install-data/$project_key/install"
+    log "Running supplemental install script for $project_key"
+    "$supp_inst"
+}
+
+# ---------------------------------------------------------------
+# Check version and if it already exists.
+# ---------------------------------------------------------------
+repo_tags="http://svn.code.sf.net/p/rosegarden/code/tags"
+
+version=$(latest_svn_repo_tag $repo_tags)
+echo "latest version: $version"
+
+[[ -e "$tools/$project_key-$version" ]] && {
+    log "$project_key-$version already exists, activating it."
+    tools_link $project_key
+    bin_links $project_key
+    supplemental_install
+    exit 0
+}
+
+prefix="$tools/$project_key-$version"
+
+# ---------------------------------------------------------------
+# Check apt Dependencies
+# ---------------------------------------------------------------
+# This will install apt packages that are required for the build.
+# These were determined empirically, and may change with time
+# and/or with Linux distribution. This will be a no-op on OSX.
+echo 'You may be asked for your password to install apt dependencies:'
+apt_deps="
+    libjack-dev
+    ladspa-sdk-dev
+    dssi-dev
+    liblo-dev
+    liblrdf-dev
+"
+echo -e "$apt_deps"
+
+install_apt_dependencies "$apt_deps"
+
+# ---------------------------------------------------------------
+# Checkout repo
+# ---------------------------------------------------------------
+checkout_latest_tag $repo_tags rosegarden-$version
+cd rosegarden-$version
+
+# ---------------------------------------------------------------
+# Configure
+# ---------------------------------------------------------------
 mkdir build
-
 cd build
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+         -DCMAKE_INSTALL_PREFIX=$prefix
 
-cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
+# ---------------------------------------------------------------
+# Build/Test
+# ---------------------------------------------------------------
+make -j"$(build_threads)"
 
-make -j$(build_threads)
+# ---------------------------------------------------------------
+# Install
+# ---------------------------------------------------------------
+make install
+
+# ---------------------------------------------------------------
+# Supplemental Installation Steps
+# ---------------------------------------------------------------
+supplemental_install
+
+# ---------------------------------------------------------------
+# Make symlinks
+# ---------------------------------------------------------------
+tools_link $project_key
+bin_links  $project_key
+
+log "Success."
