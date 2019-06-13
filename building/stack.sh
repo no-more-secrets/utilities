baacce77e8f626208bb1c22814a533b5ad9dce5d
David P. Sicilia <dpsicilia@gmail.com>
2019-06-13 12:17:13 -0400

    Adding script to install stack

--- building/stack.sh
+++ building/stack.sh
@@ -9,18 +9,52 @@ set -o pipefail
 this=$(cd $(dirname $0) && pwd)
 cd $this
 
+tools="$HOME/dev/tools"
+mkdir -p "$tools"
+
 # ---------------------------------------------------------------
 # Includes
 # ---------------------------------------------------------------
 source util.sh
 
-die "unfinished"
+project_key=stack
 
-# Implementation steps:
-#   1) download the install script with
-#         curl -sSL https://get.haskellstack.org/ | sh
-#   2) Grep that script for the STACK_VERSION variable to get version
-#   3) If not already installed, install it ~/dev/tools/stack-$STACK_VERSION
-#   4) Rename the binary from stack-$STACK_VERSION to stack
-#   6) Create link ~/dev/tools/stack-current to the folder
-#   5) Create link in ~/bin to ~/dev/tools/stack-current/stack
+script_dl="curl -sSL https://get.haskellstack.org/"
+
+# ---------------------------------------------------------------
+# Check version and if it already exists.
+# ---------------------------------------------------------------
+version=$($script_dl | grep '^STACK_VERSION=' \
+                     | cut -f2 -d=            \
+                     | tr -d "\"")
+
+log "latest version: $version"
+
+[[ "$version" =~ ^[0-9.]+$ ]] || die "unexpected version format."
+
+[[ -e "$tools/$project_key-$version" ]] && {
+    log "$project_key-$version already exists, activating it."
+    tools_link $project_key
+    bin_links $project_key
+    #supplemental_install
+    exit 0
+}
+
+
+# ---------------------------------------------------------------
+# Download / Install
+# ---------------------------------------------------------------
+dest="$HOME/dev/tools/stack-$version"
+
+$script_dl | sh -s - -d $dest
+
+mkdir "$dest/bin"
+ln -s "$dest/stack" "$dest/bin/stack"
+
+# ---------------------------------------------------------------
+# Make symlinks
+# ---------------------------------------------------------------
+tools_link $project_key
+bin_links  $project_key
+
+log "Success."
