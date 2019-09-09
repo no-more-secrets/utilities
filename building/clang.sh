865bb18f308b6f6aaba8d0b20c0f62ab4d13241f
David P. Sicilia <dpsicilia@gmail.com>
2019-09-09 12:27:25 -0400

    Add some git support to clang.sh

--- building/clang.sh
+++ building/clang.sh
@@ -18,9 +18,15 @@ gstt() {
   grep $1 | sort -V | tail -n1 | tr -d '/'
 }
 
-# Get global head revision of repo as a default.  This
-# should be fine whether trunk or tag is being built.
-rev=$(svn info --show-item=revision http://llvm.org/svn/llvm-project)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Set this flag to control whether ot use svn or git. SVN is
+# preferred because it is the primary repo and it allows us to
+# use a consistent revision number for all sub projects. It also
+# allows us to get the release candidate number. Git has the
+# advantage that it is faster to checkout and potentially more
+# reliable on wonky network connections. But if time is not a
+# concern, then SVN should be used.
+use_git=0
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Command line parameters
@@ -28,72 +34,105 @@ rev=$(svn info --show-item=revision http://llvm.org/svn/llvm-project)
 [[ "$1" =~ --trunk.* ]] && trunk=1 || trunk=0
 [[ "$1" =~ --trunk=([0-9]+) ]] && rev=${BASH_REMATCH[1]}
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+check_confirm_install() {
+  [[ -z "$suffix"  ]] && die "suffix variable not populated."
+  [[ -z "$version" ]] && die "version variable not populated."
 
-if (( trunk )); then
-    repo_root=trunk
-    release=trunk
-    [[ -z "$rev" ]] && rev=$(svn info --show-item=revision http://llvm.org/svn/llvm-project/llvm/trunk)
-    suffix=trunk-r$rev
-    candidate=
-else
-    # Get latest llvm version
-    release=$(svn ls http://llvm.org/svn/llvm-project/llvm/tags | gstt RELEASE_)
-    [[ "$release" =~ RELEASE_[0-9]+ ]] || die "release $release has unexpected form."
+  install=$HOME/dev/tools/llvm-$suffix
+  [[ -d "$install" ]] && die "$install already exists."
 
-    candidates=$(svn ls http://llvm.org/svn/llvm-project/llvm/tags/$release)
+  echo
+  echo "Will install:"
+  echo
+  echo "  LLVM/clang version: $version"
+  echo
+  echo "  To folder: $install"
+  echo
+  echo -n "Press enter to continue..."
+  read
+}
 
-    if [[ "$candidates" =~ final ]]; then
-        candidate=final
-    else
-        candidate=$(svn ls http://llvm.org/svn/llvm-project/llvm/tags/$release | gstt rc)
-    fi
+if (( use_git )); then
+  llvm_github="https://github.com/llvm-mirror"
+  git_branch=master
+  suffix=$(date +"%Y-%m-%d-%M.%H.%S")
+  version=$suffix
 
-    repo_root=tags/$release/$candidate
-    suffix=v${release/RELEASE_}.$candidate
+  git_clone() {
+    local dir=$1
+    local repo=$2
+    local target=$3
+    [[ -z "$target" ]] && target="$repo"
+    old_dir=$(pwd)
+    cd $dir
+    git clone --depth=1 --branch=$git_branch $llvm_github/$repo.git $target
+    cd "$old_dir" # do this because `cd .` --> `cd -` doesn't work.
+  }
+
+  check_confirm_install
+  cd $work
+
+  git_clone  .                       llvm
+  git_clone  llvm/tools              clang
+  git_clone  llvm/tools              lld
+  git_clone  llvm/tools/clang/tools  clang-tools-extra  extra
+  git_clone  llvm/projects           libcxx
+  git_clone  llvm/projects           libcxxabi
+  git_clone  llvm/projects           compiler-rt
+
+else # use SVN
+  # Get global head revision of repo as a default.  This
+  # should be fine whether trunk or tag is being built.
+  rev=$(svn info --show-item=revision http://llvm.org/svn/llvm-project)
+
+  if (( trunk )); then
+      repo_root=trunk
+      release=trunk
+      [[ -z "$rev" ]] && rev=$(svn info --show-item=revision http://llvm.org/svn/llvm-project/llvm/trunk)
+      suffix=trunk-r$rev
+      candidate=
+      version="$suffix"
+  else
+      # Get latest llvm version
+      release=$(svn ls http://llvm.org/svn/llvm-project/llvm/tags | gstt RELEASE_)
+      [[ "$release" =~ RELEASE_[0-9]+ ]] || die "release $release has unexpected form."
+
+      candidates=$(svn ls http://llvm.org/svn/llvm-project/llvm/tags/$release)
+
+      if [[ "$candidates" =~ final ]]; then
+          candidate=final
+      else
+          candidate=$(svn ls http://llvm.org/svn/llvm-project/llvm/tags/$release | gstt rc)
+      fi
+
+      repo_root=tags/$release/$candidate
+      suffix=v${release/RELEASE_}.$candidate
+      version="$release/$candidate"
+  fi
+
+  llvm_svn="http://llvm.org/svn/llvm-project"
+  svn_co() {
+    local dir=$1
+    local repo=$2
+    local target=$3
+    [[ -z "$target" ]] && target="$repo"
+    old_dir=$(pwd)
+    cd $dir
+    svn co -r$rev $llvm_svn/$repo/$repo_root $target
+    cd "$old_dir" # do this because `cd .` --> `cd -` doesn't work.
+  }
+
+  check_confirm_install
+  cd $work
+
+  svn_co     .                       llvm
+  svn_co     llvm/tools              cfe                clang
+  svn_co     llvm/tools              lld
+  svn_co     llvm/tools/clang/tools  clang-tools-extra  extra
+  svn_co     llvm/projects           libcxx
+  svn_co     llvm/projects           libcxxabi
+  svn_co     llvm/projects           compiler-rt
 fi
-
-install=$HOME/dev/tools/llvm-$suffix
-
-[[ -d "$install" ]] && die "$install already exists."
-
-echo
-echo "Will install:"
-echo
-echo "  LLVM/clang version: $release/$candidate"
-echo
-echo "  To folder: $install"
-echo
-echo -n "Press enter to continue..."
-read
-
-cd $work
-
-svn co -r$rev http://llvm.org/svn/llvm-project/llvm/$repo_root llvm
-
-cd llvm/tools
-svn co -r$rev http://llvm.org/svn/llvm-project/cfe/$repo_root clang
-cd ../../
-
-cd llvm/tools
-svn co -r$rev http://llvm.org/svn/llvm-project/lld/$repo_root lld
-cd ../../
-
-cd llvm/tools/clang/tools
-svn co -r$rev http://llvm.org/svn/llvm-project/clang-tools-extra/$repo_root extra
-cd ../../../../
-
-cd llvm/projects
-svn co -r$rev http://llvm.org/svn/llvm-project/libcxx/$repo_root libcxx
-cd ../..
-
-cd llvm/projects
-svn co -r$rev http://llvm.org/svn/llvm-project/libcxxabi/$repo_root libcxxabi
-cd ../..
-
-cd llvm/projects
-svn co -r$rev http://llvm.org/svn/llvm-project/compiler-rt/$repo_root compiler-rt
-cd ../..
 
 # This option would tell it where the libstdc++ is that it would use.
 # -DGCC_INSTALL_PREFIX=
@@ -116,8 +155,8 @@ cmake -G Ninja $cmake_vars ../llvm
 
 ninja
 ninja check-clang check-lld
-#ninja check-libcxx
-#ninja check-libcxxabi
+#ninja check-libcxx # FIXME enable these?
+#ninja check-libcxxabi # FIXME enable these?
 ninja install
 
 # Now create a symlink to the one we just built.
