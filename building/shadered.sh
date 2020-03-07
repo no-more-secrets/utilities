078b62c6922281ab735cc782aeea9fba07b4d126
David P. Sicilia <dpsicilia@gmail.com>
2020-03-07 16:37:59 -0500

    Add build script for SHADERed

---
+++ building/shadered.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+# ---------------------------------------------------------------
+# SHADERed Cross Platform GPU Shader Editor/IDE
+# ---------------------------------------------------------------
+set -eE
+set -o pipefail
+
+# Must be done first.
+this=$(cd $(dirname $0) && pwd)
+cd $this
+
+# ---------------------------------------------------------------
+# Includes
+# ---------------------------------------------------------------
+source util.sh
+
+# ---------------------------------------------------------------
+# Initialization
+# ---------------------------------------------------------------
+# This must be a lowercase short word with no spaces describing
+# the software being built. Folders/links will be named with
+# this.
+project_key="shadered"
+
+tools="$HOME/dev/tools"
+mkdir -p "$tools"
+
+# ---------------------------------------------------------------
+# Create Work Area
+# ---------------------------------------------------------------
+cd /tmp
+
+work=$project_key-build
+
+[[ -d $work ]] && /bin/rm -rf "$work"
+mkdir -p $work
+cd $work
+
+# ---------------------------------------------------------------
+# Dependencies
+# ---------------------------------------------------------------
+check_apt_dependencies '
+  libsdl2-dev
+  libsfml-dev
+  libglew-dev
+  libglm-dev
+  libassimp-dev
+  libgtk-3-dev
+'
+
+# ---------------------------------------------------------------
+# Functions
+# ---------------------------------------------------------------
+latest_github_repo_tag() {
+    local acct="$1"
+    local repo="$2"
+    local api_url="https://api.github.com/repos/$acct/$repo/tags"
+    # FROM: "name": "v8.1.0338",
+    # TO:   v8.1.0338
+    curl --silent $api_url | grep '"name":'       \
+                           | head -n1             \
+                           | sed 's/.*: "\(.*\)".*/\1/'
+}
+
+clone_latest_tag() {
+    local acct="$1"
+    local repo="$2"
+    # non-local
+    version=$(latest_github_repo_tag $acct $repo)
+    log "latest version of $acct/$repo: $version"
+    sleep 3
+    git clone --depth=1         \
+              --branch=$version \
+              --recursive       \
+              https://github.com/$acct/$repo
+}
+
+# ---------------------------------------------------------------
+# Check version and if it already exists.
+# ---------------------------------------------------------------
+acct="dfranx"
+repo="SHADERed"
+
+version=$(latest_github_repo_tag $acct $repo)
+
+[[ -e "$tools/$project_key-$version" ]] && {
+    log "$project_key-$version already exists, activating it."
+    tools_link $project_key
+    bin_links $project_key
+    #supplemental_install
+    exit 0
+}
+
+prefix="$tools/$project_key-$version"
+log "latest version: $version"
+
+# ---------------------------------------------------------------
+# Clone Repo
+# ---------------------------------------------------------------
+clone_latest_tag "$acct" "$repo"
+cd $repo
+
+# ---------------------------------------------------------------
+# Configure
+# ---------------------------------------------------------------
+mkdir build && cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+         -DCMAKE_INSTALL_PREFIX=$prefix
+
+# ---------------------------------------------------------------
+# Build
+# ---------------------------------------------------------------
+make -j$(build_threads)
+make install
+
+# ---------------------------------------------------------------
+# Make Links
+# ---------------------------------------------------------------
+tools_link $project_key
+bin_links $project_key
+
+# ---------------------------------------------------------------
+# Finish
+# ---------------------------------------------------------------
+log "success."
