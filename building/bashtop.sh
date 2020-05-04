e48d9774e681a79084a0533f971c5af8c86e26c1
David P. Sicilia <dpsicilia@gmail.com>
2020-05-04 16:08:33 -0400

    Add build script for bashtop.

---
+++ building/bashtop.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# ---------------------------------------------------------------
+# BashTop is to htop what htop is to top.
+# ---------------------------------------------------------------
+set -eE
+set -o pipefail
+
+# Must be done first.
+this=$(cd $(dirname $0) && pwd)
+cd $this
+
+# ---------------------------------------------------------------
+# Includes
+# ---------------------------------------------------------------
+source util.sh
+
+# ---------------------------------------------------------------
+# Initialization
+# ---------------------------------------------------------------
+# This must be a lowercase short word with no spaces describing
+# the software being built. Folders/links will be named with
+# this.
+project_key="bashtop"
+
+tools="$HOME/dev/tools"
+mkdir -p "$tools"
+
+# ---------------------------------------------------------------
+# Create Work Area
+# ---------------------------------------------------------------
+cd /tmp
+
+work=$project_key-build
+
+[[ -d $work ]] && /bin/rm -rf "$work"
+mkdir -p $work
+cd $work
+
+# ---------------------------------------------------------------
+# Functions
+# ---------------------------------------------------------------
+latest_github_repo_tag() {
+    local acct="$1"
+    local repo="$2"
+    local api_url="https://api.github.com/repos/$acct/$repo/tags"
+    # FROM: "name": "v8.1.0338",
+    # TO:   v8.1.0338
+    curl --silent $api_url | grep '"name":'       \
+                           | head -n1             \
+                           | sed 's/.*: "\(.*\)".*/\1/'
+}
+
+clone_latest_tag() {
+    local acct="$1"
+    local repo="$2"
+    # non-local
+    version=$(latest_github_repo_tag $acct $repo)
+    log "latest version of $acct/$repo: $version"
+    sleep 3
+    git clone --depth=1         \
+              --branch=$version \
+              --recursive       \
+              https://github.com/$acct/$repo
+}
+
+# ---------------------------------------------------------------
+# Check version and if it already exists.
+# ---------------------------------------------------------------
+acct="aristocratos"
+repo="bashtop"
+
+version=$(latest_github_repo_tag $acct $repo)
+
+[[ -e "$tools/$project_key-$version" ]] && {
+    log "$project_key-$version already exists, activating it."
+    tools_link $project_key
+    bin_links $project_key
+    #supplemental_install
+    exit 0
+}
+
+prefix="$tools/$project_key-$version"
+log "latest version: $version"
+
+# ---------------------------------------------------------------
+# Clone Repo
+# ---------------------------------------------------------------
+clone_latest_tag "$acct" "$repo"
+cd $repo
+
+# ---------------------------------------------------------------
+# Install
+# ---------------------------------------------------------------
+mkdir -p $prefix/bin
+cp bashtop $prefix/bin
+cp -r themes $prefix
+
+# ---------------------------------------------------------------
+# Make Links
+# ---------------------------------------------------------------
+tools_link $project_key
+bin_links $project_key
+
+# ---------------------------------------------------------------
+# Finish
+# ---------------------------------------------------------------
+log "success."
