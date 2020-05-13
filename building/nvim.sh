e5103906749bae06d3588645a243e1caf1a38c03
David P. Sicilia <dpsicilia@gmail.com>
2020-05-13 11:14:32 -0400

    Add build script for neovim

---
+++ building/nvim.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+# ---------------------------------------------------------------
+# NeoVim
+# ---------------------------------------------------------------
+set -eE
+set -o pipefail
+
+# Must be done first.
+this=$(cd $(dirname $0) && pwd)
+cd $this
+
+# ---------------------------------------------------------------
+# Includes
+# ---------------------------------------------------------------
+source util.sh
+
+# ---------------------------------------------------------------
+# Initialization
+# ---------------------------------------------------------------
+# This must be a lowercase short word with no spaces describing
+# the software being built. Folders/links will be named with
+# this.
+project_key="nvim"
+
+tools="$HOME/dev/tools"
+mkdir -p "$tools"
+
+# ---------------------------------------------------------------
+# Create Work Area
+# ---------------------------------------------------------------
+cd /tmp
+
+work=$project_key-build
+
+[[ -d $work ]] && /bin/rm -rf "$work"
+mkdir -p $work
+cd $work
+
+# ---------------------------------------------------------------
+# Functions
+# ---------------------------------------------------------------
+latest_github_repo_tag() {
+    local acct="$1"
+    local repo="$2"
+    local api_url="https://api.github.com/repos/$acct/$repo/tags"
+    # FROM: "name": "v8.1.0338",
+    # TO:   v8.1.0338
+    curl --silent $api_url | grep '"name":'       \
+                           | head -n1             \
+                           | sed 's/.*: "\(.*\)".*/\1/'
+}
+
+clone_latest_tag() {
+    local acct="$1"
+    local repo="$2"
+    # non-local
+    version=$(latest_github_repo_tag $acct $repo)
+    log "latest version of $acct/$repo: $version"
+    sleep 3
+    git clone --depth=1         \
+              --branch=$version \
+              --recursive       \
+              https://github.com/$acct/$repo
+}
+
+# ---------------------------------------------------------------
+# Check version and if it already exists.
+# ---------------------------------------------------------------
+acct="neovim"
+repo="neovim"
+
+version=$(latest_github_repo_tag $acct $repo)
+
+[[ -e "$tools/$project_key-$version" ]] && {
+    log "$project_key-$version already exists, activating it."
+    tools_link $project_key
+    bin_links $project_key
+    #supplemental_install
+    exit 0
+}
+
+prefix="$tools/$project_key-$version"
+log "latest version: $version"
+
+# ---------------------------------------------------------------
+# Clone Repo
+# ---------------------------------------------------------------
+clone_latest_tag "$acct" "$repo"
+cd $repo
+
+# ---------------------------------------------------------------
+# Build
+# ---------------------------------------------------------------
+# NeoVim has a CMake-based build, but the directions say to use
+# the Makefile, which also appears to do other things like down-
+# load third-party dependencies.
+make CMAKE_BUILD_TYPE=RelWithDebInfo \
+     CMAKE_INSTALL_PREFIX=$prefix    \
+     -j$(build_threads)              \
+     install
+
+# ---------------------------------------------------------------
+# First-time vim-transition
+# ---------------------------------------------------------------
+# This is required to allow for python extentions.
+python3 -m pip install --user --upgrade pynvim
+
+# ---------------------------------------------------------------
+# Make Links
+# ---------------------------------------------------------------
+tools_link $project_key
+bin_links $project_key
+
+# ---------------------------------------------------------------
+# Finish
+# ---------------------------------------------------------------
+log "success."
