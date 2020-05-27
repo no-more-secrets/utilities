6608055052f7c955e53bd1416c7e61bd670c50b4
David P. Sicilia <dpsicilia@gmail.com>
2020-05-27 11:43:50 -0400

    Add some comments

--- dconf/fix-idle-delay.sh
+++ dconf/fix-idle-delay.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# 1. Edit /etc/dconf/db/site.d/locks/screensaver and remove all
+#    the lines containing "idle-delay".
+# 2. sudo dconf update
+# 3. Run this script.
+
 echo "You may be asked for your password:"
 
 sudo true
