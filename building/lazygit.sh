533f1b7fec8f08820a06af0e0c5a8856a9877c99
David P. Sicilia <dpsicilia@gmail.com>
2020-06-08 18:55:29 -0400

    Change lazygit script to properly build lazygit.

--- building/lazygit.sh
+++ building/lazygit.sh
@@ -26,27 +26,95 @@ tools="$HOME/dev/tools"
 mkdir -p "$tools"
 
 # ---------------------------------------------------------------
+# Create Work Area
+# ---------------------------------------------------------------
+cd /tmp
+
+work=$project_key-build
+
+[[ -d $work ]] && /bin/rm -rf "$work"
+mkdir -p $work
+cd $work
+
+# ---------------------------------------------------------------
 # Dependencies
 # ---------------------------------------------------------------
-export GOPATH=~/dev/go
+export GOPATH=/tmp/go-path-tmp
+[[ -e "$GOPATH" ]] && rm -r "$GOPATH"
+mkdir -p "$GOPATH"
 
 install_apt_dependencies '
   golang
 '
 
 # ---------------------------------------------------------------
+# Functions
+# ---------------------------------------------------------------
+latest_github_repo_tag() {
+    local acct="$1"
+    local repo="$2"
+    local api_url="https://api.github.com/repos/$acct/$repo/tags"
+    # FROM: "name": "v8.1.0338",
+    # TO:   v8.1.0338
+    curl --silent $api_url | grep '"name":'       \
+                           | head -n1             \
+                           | sed 's/.*: "\(.*\)".*/\1/'
+}
+
+clone_latest_tag() {
+    local acct="$1"
+    local repo="$2"
+    # non-local
+    version=$(latest_github_repo_tag $acct $repo)
+    log "latest version of $acct/$repo: $version"
+    sleep 3
+    git clone --depth=1         \
+              --branch=$version \
+              --recursive       \
+              https://github.com/$acct/$repo
+}
+
+# ---------------------------------------------------------------
+# Check version and if it already exists.
+# ---------------------------------------------------------------
+acct="jesseduffield"
+repo="lazygit"
+
+version=$(latest_github_repo_tag $acct $repo)
+
+[[ -e "$tools/$project_key-$version" ]] && {
+    log "$project_key-$version already exists, activating it."
+    tools_link $project_key
+    bin_links $project_key
+    #supplemental_install
+    exit 0
+}
+
+prefix="$tools/$project_key-$version"
+log "latest version: $version"
+
+# ---------------------------------------------------------------
+# Clone Repo
+# ---------------------------------------------------------------
+clone_latest_tag "$acct" "$repo"
+cd $repo
+
+# ---------------------------------------------------------------
 # Build
 # ---------------------------------------------------------------
-go get github.com/jesseduffield/lazygit
-prefix=$tools/$project_key
+go build
+./lazygit --help
+
+# ---------------------------------------------------------------
+# Install
+# ---------------------------------------------------------------
 mkdir -p $prefix/bin
-cp $GOPATH/bin/lazygit $prefix/bin
+cp lazygit $prefix/bin
 
 # ---------------------------------------------------------------
 # Make Links
 # ---------------------------------------------------------------
-rm -f $tools/lazygit-current
-ln -s $tools/lazygit $tools/lazygit-current
+tools_link $project_key
 bin_links $project_key
 
 # ---------------------------------------------------------------
