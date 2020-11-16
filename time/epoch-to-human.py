dcfda0cb3f5547fba9158315ccbb3342fb0e9240
David P. Sicilia <dpsicilia@gmail.com>
2020-11-16 14:28:47 -0500

    Fix issues with epoch-to-human script.

--- time/epoch-to-human.py
+++ time/epoch-to-human.py
@@ -9,23 +9,23 @@ if len( sys.argv ) <= 1:
 
 t = sys.argv[1]
 
-t_int = int( t )
+t_int = long( t )
 
-if t_int <= 9999999999:
+if len( t ) <= 10:
     # seconds
     seconds = t_int
     microseconds = 0
-elif t_int <= 9999999999999:
+elif len( t ) <= 13:
     # milliseconds
     seconds = t_int//1000
     microseconds = (t_int % 1000)*1000
-elif t_int <= 9999999999999999:
+elif len( t ) <= 16:
     # microseconds
     seconds = t_int//1000000
     microseconds = t_int % 1000000
 
-local = time.strftime('%Y-%m-%d %H:%M:%S.%06d', time.localtime(seconds))
-utc   = time.strftime('%Y-%m-%d %H:%M:%S.%06d', time.gmtime(seconds))
+local = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(seconds))
+utc   = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(seconds))
 
 print( "EST: {}.{:06d}".format( local, microseconds ) )
 print( "UTC: {}.{:06d}".format( utc,   microseconds ) )
