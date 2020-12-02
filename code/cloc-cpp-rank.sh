6ca982420a6df0672318c2534d2804de0cdcc040
David P. Sicilia <dpsicilia@gmail.com>
2020-12-02 17:54:41 -0500

    fix bug in cloc-cpp-rank script.

--- code/cloc-cpp-rank.sh
+++ code/cloc-cpp-rank.sh
@@ -11,8 +11,8 @@
       if ! { cloc $d 2>/dev/null | grep -E '^(C/)?C\+\+'; }; then
           echo _ _ _ _ 0
       fi
-  done | awk '{print $6, $1}' \
-       | sort -nr             \
+  done | awk '{print $NF, $1}' \
+       | sort -nr              \
        | awk '{print $2, $1}'
   echo --------------------------------------------- -------------
 } | awk '{ count += $2; print } END { print "Total", count }' \
