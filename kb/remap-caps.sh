2a2b3d4298ec6c7e24cb53264d9aff682a933a3d
David P. Sicilia <dpsicilia@gmail.com>
2021-07-11 17:52:54 -0400

    Add some keyboard utils

--- kb/remap-caps.sh
+++ kb/remap-caps.sh
@@ -3,6 +3,6 @@ set -e
 
 # Try one each of these.
 
-# setxkbmap -option caps:ctrl_modifier
+setxkbmap -option caps:ctrl_modifier
 
 # gsettings set org.gnome.desktop.input-sources xkb-options "['caps:ctrl_modifier']"
---
+++ kb/set-repeat-rate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# sets to default
+# xset r rate
+
+xset r rate 200 100
