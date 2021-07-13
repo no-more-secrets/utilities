195d800f2b1a87aad89cbddea5e74e06b05f02a4
David P. Sicilia <dpsicilia@gmail.com>
2021-07-13 19:29:26 -0400

    Update controls console.

--- consoles/layouts/basic-controls.lua
+++ consoles/layouts/basic-controls.lua
@@ -1,6 +1,6 @@
 return vertical{
   [1]=horizontal{
-    [1]=command{ 'alsamixer' },
+    [1]=command{ 'watch -c -n20 "nmcli --color=yes dev wifi"' },
     [2]=vertical{
       [1]=command{ 'bluetoothctl' },
       [2]=command{ '~/dev/utilities/sound/volume-ctrl-repl.sh' }
@@ -9,7 +9,7 @@ return vertical{
   [2]=horizontal{
     [1]=command{ 'htop' },
     [2]=vertical{
-      [1]=command{ 'watch -c -n20 "nmcli --color=yes dev wifi"' },
+      [1]=command{ 'bashtop' },
       [2]=command{ 'fish' }
     },
   },
