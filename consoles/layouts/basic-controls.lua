6f12f6de83accd73014d1fad10abfc9b86b14e27
David P. Sicilia <dpsicilia@gmail.com>
2021-07-13 23:20:27 -0400

    Tweak to console.

--- consoles/layouts/basic-controls.lua
+++ consoles/layouts/basic-controls.lua
@@ -3,14 +3,14 @@ return vertical{
     [1]=command{ 'watch -c -n20 "nmcli --color=yes dev wifi"' },
     [2]=vertical{
       [1]=command{ 'bluetoothctl' },
-      [2]=command{ 'pulsemixer' }
+      [2]=vertical{
+        [1]=command{ 'pulsemixer' },
+        [2]=command{ 'fish' }
+      }
     }
   },
   [2]=horizontal{
     [1]=command{ 'htop' },
-    [2]=vertical{
-      [1]=command{ 'bashtop' },
-      [2]=command{ 'fish' }
-    },
+    [2]=command{ 'bashtop' }
   },
 }
