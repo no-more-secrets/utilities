8c6f5af45c7a301a68d36f26a676efb9e820895d
David P. Sicilia <dpsicilia@gmail.com>
2021-07-21 13:02:44 -0400

    Tweak to mouse pointer acceleration script.

--- mouse/set-pointer-acceleration.sh
+++ mouse/set-pointer-acceleration.sh
@@ -9,7 +9,7 @@ id="$1"
 if [[ -z "$id" ]]; then
   xinput list
   echo
-  echo -n "Input the id of the mouse (or rerun script with id as argument): "
+  echo -n "Input the id of the mouse or touchpad (or rerun script with id as argument): "
   read id
   (( id > 0 )) || die "invalid id."
 fi
@@ -18,9 +18,9 @@ prop_name='libinput Accel Speed'
 
 { xinput list-props $id | grep "$prop_name"; } || \
   die "could not find property '$prop_name' on device $id. " \
-      "Run \`xinput list-props $id\` to find the property name of the mouse acceleration."
+      "Run \`xinput list-props $id\` to find the property name of the mouse or touchpad acceleration."
 
-echo "Select abs value of mouse acceleration (0=normal):"
+echo "Select abs value of pointer acceleration (0=normal):"
 value=$(echo " .9 (fastest)
  .7
  .5
@@ -35,5 +35,5 @@ value=$(echo " .9 (fastest)
 
 [[ ! -z "$value" ]]
 
-echo "setting mouse acceleration to $value."
+echo "setting pointer acceleration to $value."
 xinput --set-prop $id "$prop_name" $value
