40be7565b63fb95ea7a19a5e14f12394b66086f5
David P. Sicilia <dpsicilia@gmail.com>
2021-07-23 11:54:53 -0400

    Moving some things around.

--- consoles/layouts/basic-controls.lua
+++ consoles/layouts/basic-controls.lua
@@ -3,10 +3,7 @@ return vertical{
     [1]=command{ 'watch -c -n20 "nmcli --color=yes dev wifi"' },
     [2]=vertical{
       [1]=command{ 'bluetoothctl' },
-      [2]=vertical{
-        [1]=command{ 'pulsemixer' },
-        [2]=command{ 'fish' }
-      }
+      [2]=command{ 'pulsemixer' },
     }
   },
   [2]=horizontal{
