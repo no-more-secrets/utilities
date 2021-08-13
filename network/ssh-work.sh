f4f0ca41ac91868737ab945599612bcf9c655749
David P. Sicilia <dpsicilia@gmail.com>
2021-08-13 14:29:25 -0400

    Simplification to script.

--- network/ssh-work.sh
+++ network/ssh-work.sh
@@ -10,11 +10,7 @@ sock=/tmp/ssh-auth.sock
 
 export SSH_AUTH_SOCK=$sock
 
-# Trick used to get bash to source a file before the prompt.
 ssh -t localhost -p 43022 '
-  # bash command must be on same line as variable setting.
-  PROMPT_COMMAND="
-    source /tmp/ssh-source.sh
-    unset PROMPT_COMMAND
-  " exec bash
+  source /tmp/ssh-source.sh
+  fish
 '
