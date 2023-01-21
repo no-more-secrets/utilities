889eb1039c29d67a079526d18fa2eb417460c4d7
David P. Sicilia <dpsicilia@gmail.com>
2023-01-21 10:05:12 -0500

    Decrease frequency of wifi refresh on console.

--- consoles/layouts/basic-controls.lua
+++ consoles/layouts/basic-controls.lua
@@ -1,6 +1,6 @@
 return vertical{
   [1]=horizontal{
-    [1]=command{ 'watch -c -n20 "nmcli --color=yes dev wifi list --rescan yes"' },
+    [1]=command{ 'watch -c -n240 "nmcli --color=yes dev wifi list --rescan yes"' },
     [2]=vertical{
       [1]=command{ 'bluetoothctl' },
       [2]=command{ 'pulsemixer' },
