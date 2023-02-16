91ea0a32761ac9739bd9fb9c3913ba56dbf3fcb2
David P. Sicilia <dpsicilia@gmail.com>
2023-02-16 11:02:47 -0500

    Add some scripts related to patching the col1 binary.

---
+++ colonization/use-fixed.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+set -e
+set -x
+# This will enable the patched version that has the RNG generator
+# fixed. That process is first fix described here:
+#
+#   https://civilization.fandom.com/wiki/Bugs_(Col)
+#
+# But note that, as described there, it will cause the map gener-
+# ation to be the same each time. So when starting a new map one
+# should use-orig.sh, then when playing or experimenting one
+# should use-fixed.sh. This is likely because the original code
+# will re-seed the RNG with the current time each time it is
+# called (leading to non-random "streaks"), whereas the patched
+# version prevents it from doing that, leading to more random be-
+# havior but without ever randomly seeding it at the start of the
+# program.
+cp VICEFIX.EXE VICEROY.EXE
---
+++ colonization/use-orig.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+set -e
+set -x
+# This will enable the non-patched version that has the buggy RNG
+# generator fixed. That process is first fix described here:
+#
+#   https://civilization.fandom.com/wiki/Bugs_(Col)
+#
+# Note that, as described there, this version is needed in order
+# to get random map generations. So when starting a new map one
+# should use-orig.sh, then when playing or experimenting one
+# should use-fixed.sh. This is likely because the original code
+# will re-seed the RNG with the current time each time it is
+# called (leading to non-random "streaks"), whereas the patched
+# version prevents it from doing that, leading to more random be-
+# havior but without ever randomly seeding it at the start of the
+# program.
+cp VICEORIG.EXE VICEROY.EXE
