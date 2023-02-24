af7562037d304eba4f5551d67ac57fe4d899ff95
David P. Sicilia <dpsicilia@gmail.com>
2023-02-23 22:42:20 -0500

    Add comment to script.

--- keyboard/disable-workspace-switch-animation.sh
+++ keyboard/disable-workspace-switch-animation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# By default, when switching workspaces in gnome, it will animate
+# them sliding. This will disable that.
 set -eo pipefail
 
 gsettings set org.gnome.desktop.interface enable-animations false
