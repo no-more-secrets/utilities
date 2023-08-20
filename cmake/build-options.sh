39372d2eb0ca57b603dd7a2ff5e1ecc38152c63b
David P. Sicilia <dpsicilia@gmail.com>
2023-08-20 09:32:51 -0400

    Change default development build to release-asan.

--- cmake/build-options.sh
+++ cmake/build-options.sh
@@ -66,7 +66,7 @@ update() {
   git submodule update --init
 }
 
-restore() { cmc --clang --lld --libstdcxx --release --cached; }
+restore() { cmc --clang --lld --libstdcxx --release --asan --cached; }
 
 case "$answer" in
   "Update & Build (release)")
