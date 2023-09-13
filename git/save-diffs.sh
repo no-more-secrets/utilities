d564ca631946632161b5132b792b6a28fb261a37
David P. Sicilia <dpsicilia@gmail.com>
2023-09-13 10:25:01 -0400

    Add script for saving local diffs to the stash.

---
+++ git/save-diffs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script will save the current changes to the stash.
+set -eo pipefail
+
+# There is also the "git stash create" + "git stash store" method
+# which won't touch the timestamps on the files, but it seems
+# that unfortunately it doesn't work with untracked files.
+git stash push --include-untracked -m "backup of local changes: $(date)"
+git stash apply
