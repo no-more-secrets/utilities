d7a35debf4926163984c1b2fa55469f913b1538e
David P. Sicilia <dpsicilia@gmail.com>
2023-09-23 17:55:10 -0400

    Don't touch timestamps when saving to stash.

---
+++ git/stash-diffs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script will stash the current changes to the stash without
+# touching their timestamps.
+set -eo pipefail
+
+# We use the "git stash create" + "git stash store" method in-
+# stead of "git stash push" + "git stash apply" because the
+# former will not touch the timestamsp on the files. However, the
+# former has the disadvantage that it does not work with un-
+# tracked files.
+hash=$(git stash create)
+git stash store -m "backup of local changes: $(date)" "$hash"
+echo ---------------------------------------------------------------------------
+git stash list | head
