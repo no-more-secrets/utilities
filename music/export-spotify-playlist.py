65b6dc3199d2a2ab5cbcbfacb0805932f55e7a19
David P. Sicilia <dpsicilia@gmail.com>
2026-06-13 13:29:51 -0400

    Update playlist

--- music/export-spotify-playlist.py
+++ music/export-spotify-playlist.py
@@ -1,33 +1,84 @@
-import sys, json, spotipy
+import sys
+import json
+import spotipy
+
 try:
-  import keys
+    import keys
 except:
-  print( 'ERROR: keys.py.asc must first be decrypted. sync.sh is supposed to do this.', file=sys.stderr )
-  exit( 1 )
+    print(
+        "ERROR: keys.py.asc must first be decrypted. sync.sh is supposed to do this.",
+        file=sys.stderr,
+    )
+    sys.exit(1)
 
-playlist_url = sys.argv[1]
-assert playlist_url
+playlist = sys.argv[1]
+assert playlist
 
-client       = None
+auth_mgr = spotipy.oauth2.SpotifyOAuth(
+    client_id=keys.clientId,
+    client_secret=keys.clientSecret,
+    redirect_uri="http://127.0.0.1:8080/callback",
+    scope="playlist-read-private playlist-read-collaborative",
+    open_browser=True,
+)
 
-print( f'clientId:     {keys.clientId}', file=sys.stderr )
-print( f'clientSecret: {keys.clientSecret}', file=sys.stderr )
+client = spotipy.Spotify(auth_manager=auth_mgr)
 
-cred_mgr     = spotipy.oauth2.SpotifyClientCredentials( client_id=keys.clientId, client_secret=keys.clientSecret )
-client       = spotipy.Spotify( client_credentials_manager=cred_mgr )
+# Accept either a playlist URL or a playlist ID.
+playlist_id = client.playlist(playlist, fields="id")["id"]
 
 songs = []
-while True:
-  playlistResponse = client.playlist_tracks( playlist_url, offset=len(songs) )
-  for songEntry in playlistResponse['items']:
-    url = 'https://open.spotify.com/track/' + songEntry['track']['id']
-    rawTrackMeta = client.track( url )
-    songs.append(dict(song_name=rawTrackMeta['name'],
-                      album_artists=[artist['name'] for artist in rawTrackMeta['album']['artists']],
-                      contrib_artists=[artist['name'] for artist in rawTrackMeta['artists']],
-                      album_name=rawTrackMeta['album']['name']))
-  # check if more tracks are to be passed        
-  if not playlistResponse['next']:
-    break
+offset = 0
 
-print( json.dumps( songs, indent=2 ) )
+fields = (
+    "items("
+        "item("
+            "type,"
+            "name,"
+            "artists(name),"
+            "album("
+                "name,"
+                "artists(name)"
+            ")"
+        ")"
+    "),"
+    "next,"
+    "limit"
+)
+
+while True:
+    playlist_response = client._get(
+        f"playlists/{playlist_id}/items",
+        limit=100,
+        offset=offset,
+        fields=fields,
+        additional_types="track",
+    )
+
+    for song_entry in playlist_response["items"]:
+        track = song_entry.get("item")
+
+        if not track or track.get("type") != "track":
+            continue
+
+        songs.append(
+            {
+                "song_name": track["name"],
+                "album_artists": [
+                    artist["name"]
+                    for artist in track["album"]["artists"]
+                ],
+                "contrib_artists": [
+                    artist["name"]
+                    for artist in track["artists"]
+                ],
+                "album_name": track["album"]["name"],
+            }
+        )
+
+    if not playlist_response["next"]:
+        break
+
+    offset += playlist_response["limit"]
+
+print(json.dumps(songs, indent=2))
